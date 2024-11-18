78274ca14db774afe7a982f16cf624c36cef84c0
Alastair Garner <garneralastair@gmail.com>
2024-11-18 20:25:52 +0000

    Working version for a bit

--- keyboards/beekeeb/piantor/keymaps/alastair/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/alastair/keymap.c
@@ -26,23 +26,28 @@ combo_t key_combos[] = {
 #define OSM_SFT OSM(MOD_LSFT)
 #define OSL_SYM OSL(_SYM)
 #define OSL_NAV OSL(_NAV)
+#define MO_NAV MO(_NAV)
 #define MO_NUM MO(_NUM)
+#define TO_NAV TO(_NAV)
+#define TO_NUM TO(_NUM)
+#define TO_BASE TO(_BASE)
 
 #define GUI_ENT GUI_T(KC_ENT)
 #define LOW_TAB LT(_LOWER, KC_TAB)
 #define RSE_BSP LT(_RAISE, KC_BSPC)
 
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
      * ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
-     * │Esc│ Q │ W │ E │ R │ T │       │ Y │ U │ I │ O │ P │ - │
+     * │Esc│ Q │ W │ E │ R │ T │       │ Y │ U │ I │ O │ P │Bsp│
      * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
      * │Tab│ A │ S │ D │ F │ G │       │ H │ J │ K │ L │ ' │Ent│
      * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-     * |NUM│ Z │ X │ C │ V │ B │       │ N │ M │ , │ . │ / │ ` │
+     * | ` │ Z │ X │ C │ V │ B │       │ N │ M │ , │ . │ / │   │
      * └───┴───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┴───┘
      *               ┌───┐                   ┌───┐
-     *               |Nav|                   |   |
+     *               |NUM|                   |NAV|
      *               └───├───┐           ┌───┤───┘
      *                   |Cmd├───┐   ┌───┤Spc|
      *                   └───┤SYM│   │Sft├───┘
@@ -52,23 +57,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // Top left
         KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T,
         // Top right
-        KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS,
+        KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
         // Middle left
         KC_TAB, MT(MOD_LSFT, KC_A), MT(MOD_LCTL, KC_S), MT(MOD_LALT, KC_D), MT(MOD_LGUI, KC_F), KC_G,
         // Middle right
         KC_H, KC_J, KC_K, KC_L, KC_QUOT, KC_ENT,
         // Bottom left
-        MO_NUM, KC_Z, KC_X, KC_C, KC_V, KC_B,
+        KC_GRV, KC_Z, KC_X, KC_C, KC_V, KC_B,
         // Bottom right
-        KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_GRV,
+        KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, TO_NUM,
         // Thumb left
-        OSL_NAV, KC_LGUI, OSL_SYM,
+        MO_NUM, KC_LGUI, OSL_SYM,
         // Thumb right
-        OSM_SFT, KC_SPC, KC_BSPC),
+        OSM_SFT, KC_SPC, KC_TRNS),
 
     /*
      * ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
-     * │   │BLD│   │   │   │   │       │ { │ [ │ ] │ } │   │   │
+     * │   │   │   │   │   │   │       │ { │ [ │ ] │ } │   │   │
      * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
      * │   │SFT│CTL│ALT│GUI│   │       │Lft│Dwn│Up │Rig│   │   │
      * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
@@ -82,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [_NAV] = LAYOUT_split_3x6_3(
         // Top left
-        KC_TRNS, QK_BOOT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         // Top right
         LSG(KC_LBRC), LGUI(KC_LBRC), LGUI(KC_RBRC), LSG(KC_RBRC), KC_TRNS, KC_TRNS,
         // Middle left
@@ -100,7 +105,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /*
      * ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
-     * │   │   │   │   │   │   │       │   │ 7 │ 8 │ 9 │   │   │
+     * │   │BLD│   │   │   │   │       │   │ 7 │ 8 │ 9 │   │   │
      * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
      * │   │   │   │   │   │   │       │   │ 4 │ 5 │ 6 │   │   │
      * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
@@ -114,7 +119,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [_NUM] = LAYOUT_split_3x6_3(
         // Top left
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, QK_BOOT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         // Top right
         KC_TRNS, KC_7, KC_8, KC_9, KC_TRNS, KC_TRNS,
         // Middle left
@@ -162,31 +167,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // Thumb right
         KC_ESC, KC_TRNS, KC_DEL),
 };
-
-// Send function key when layer is changed
-// Shouldn't use F14, F15 or F20, as these are used by the OS
-// layer_state_t layer_state_set_user(layer_state_t state) {
-//     switch (get_highest_layer(state)) {
-//     case _BASE:
-//         SEND_STRING(SS_TAP(X_F20));
-//         break;
-//     case _NAV:
-//         SEND_STRING(SS_TAP(X_F17));
-//         break;
-//     case _NUM:
-//         SEND_STRING(SS_TAP(X_F18));
-//         break;
-//     case _SYM:
-//         SEND_STRING(SS_TAP(X_F19));
-//         break;
-//     default: //  for any other layers, or the default layer
-//         SEND_STRING(SS_TAP(X_F20));
-//         break;
-//     }
-//   return state;
-// }
-
-// layer_state_t layer_state_set_user(layer_state_t state) {
-//     return update_tri_layer_state(state, _SYM, _NAV, _NUM);
-// }
-
